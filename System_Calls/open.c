e9d7519975d516604ed5ecda20d881583d272071
Vikeesalunkhe <Vikeesalunkhe2002@gmail.com>
2026-07-04 23:24:36 +0530

    Add comments to clarify file descriptor usage and open flags in system call examples

--- System_Calls/open.c
+++ System_Calls/open.c
@@ -5,6 +5,11 @@ int main(){
 
     //open the f1.txt in write mode 
 
+    //fd : file descriptor is an integer value which is used to identify a file or a socket. It is returned by the open() system call and is used in subsequent read(), write(), and close() system calls to perform operations on the file or socket.
+    //O_CREAT : This flag is used to create a new file if it does not exist. If the file already exists, this flag has no effect.
+    //O_WRONLY : This flag is used to open the file in write-only mode.
+    //0777 : This is the file permission mode, which specifies the read, write, and execute permissions for the file.
+    
     int fd = open("f1.txt", O_CREAT | O_WRONLY, 0777);
 
     printf("FD -> %d\n", fd);
